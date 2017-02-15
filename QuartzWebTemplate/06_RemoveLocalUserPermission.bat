c558e22dd55c917b7060e5cfbbe3dcdd54dcc35e
cDoru <cioclea.doru@gmail.com>
2017-02-15 20:57:14 +0200

    Update 06_RemoveLocalUserPermission.bat
    changed container name

--- QuartzWebTemplate/06_RemoveLocalUserPermission.bat
+++ QuartzWebTemplate/06_RemoveLocalUserPermission.bat
@@ -1,4 +1,4 @@
 @echo off
 REM Remove Local User Account Permission to the "QuartzPocContainer" Key Container
-%windir%\Microsoft.NET\Framework64\v4.0.30319\aspnet_regiis.exe -pr PepperDashContainer "%userdomain%\%username%"
+%windir%\Microsoft.NET\Framework64\v4.0.30319\aspnet_regiis.exe -pr QuartzPocContainer "%userdomain%\%username%"
 pause
