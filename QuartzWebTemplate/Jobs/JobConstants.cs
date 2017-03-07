ebfdd4dea114efd637d8e70ad5ce33fed32bfde6
Doru Cioclea <cioclea.doru@gmail.com>
2017-03-07 11:38:13 +0200

    get rid of JobConstants.cs

--- QuartzWebTemplate/Jobs/JobConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace QuartzWebTemplate.Jobs
-{
-    public class JobConstants
-    {
-        public const string FailingHelloJobKey = "hello-fail";
-        public const string HelloJobKey = "hello";
-    }
-}
