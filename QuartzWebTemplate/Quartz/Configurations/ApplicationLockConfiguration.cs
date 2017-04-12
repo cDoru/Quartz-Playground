6292d1d6270ac59a9f107cb3ac19e50a0c5b8443
Doru Cioclea <cioclea.doru@gmail.com>
2017-04-12 11:10:27 +0300

    add applicationlock configuration for ef

---
+++ QuartzWebTemplate/Quartz/Configurations/ApplicationLockConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using QuartzWebTemplate.Quartz.Entities;
+
+namespace QuartzWebTemplate.Quartz.Configurations
+{
+    public class ApplicationLockConfiguration : EntityTypeConfiguration<ApplicationLock>
+    {
+        private const string TableName = "ApplicationLock";
+
+        public ApplicationLockConfiguration()
+            : this("dbo")
+        {
+
+        }
+
+        private ApplicationLockConfiguration(string schema)
+        {
+            ToTable(TableName, schema);
+
+            HasKey(x => x.Id);
+
+            Property(x => x.UtcTimestamp).HasColumnName("UtcTimestamp").HasColumnType("datetime").IsRequired().HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+            Property(x => x.LockName).HasColumnName("LockName").HasColumnType("nvarchar").HasMaxLength(256).IsRequired().HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+        }
+    }
+}
